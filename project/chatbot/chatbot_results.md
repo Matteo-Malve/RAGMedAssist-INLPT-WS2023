ffcae710acfd04300fb0f4e8278450b2af7cf201
friebolin <friebolin@cl.uni-heidelberg.de>
2024-02-18 11:00:16 +0100

    [Sandra] Update code & config file for testing hybrid search parameters

---
+++ project/chatbot/chatbot_results.md
@@ -0,0 +1,33 @@
+## Query 0:
+*Is occupational outcome in bipolar disorder predicted by premorbid functioning and intelligence?*
+
+**Execution Time:**
+17.32 seconds on cuda using mistralai/Mistral-7B-Instruct-v0.1.
+
+### Response:
+ Based on the provided context, it appears that there is some debate about whether premorbid functioning and intelligence play a role in determining occupational outcome in bipolar disorder. Some studies suggest that premorbid functioning and intelligence do not have a significant impact on occupational outcome in bipolar disorder, while others suggest that they may play a role. For example, one study found that lower premorbid intelligence was associated with a greater risk of receiving disability benefits in bipolar disorder patients, but another study found that premorbid intelligence did not explain the higher risk of receiving disability benefits in bipolar disorder patients. Additionally, some studies suggest that the persistence of severe clinical symptoms may be more important in determining occupational outcome in bipolar disorder than global cognitive functioning. Overall, more research is needed to fully understand the relationship between premorbid functioning and intelligence and occupational outcome in bipolar disorder.
+
+---
+
+## Query 1:
+*What are the effects of α1-antitrypsin (AAT) treatment on chronic fatigue syndrome (CFS) based on a case study involving a 49-year-old woman?*
+
+**Execution Time:**
+14.45 seconds on cuda using mistralai/Mistral-7B-Instruct-v0.1.
+
+### Response:
+ Based on the provided abstract, it appears that a 49-year-old woman with chronic fatigue syndrome (CFS) was treated with intravenous infusions of a human plasmaderived alpha1 antitrypsin (AAT) concentrate for 8 consecutive weeks. The treatment resulted in improvements in maximal workload, working memory scores, and perceptual organization scores. The patient's monocyte elastase, a regulator of inflammatory processes, also decreased to a normal range. These findings suggest a possible role for AAT in the treatment of CFS.
+
+---
+
+## Query 2:
+*What are your favorite comedies?*
+
+**Execution Time:**
+8.88 seconds on cuda using mistralai/Mistral-7B-Instruct-v0.1.
+
+### Response:
+ I am sorry, but I cannot provide an answer to your question "What are your favorite comedies?" as there is no information in the given context about my personal preferences or knowledge of comedy.
+
+---
+
--- project/chatbot/custom_chatbot.py
+++ project/chatbot/custom_chatbot.py
@@ -27,14 +27,16 @@ from transformers import (
 
 
 from langchain import HuggingFacePipeline, hub
+from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
-from langchain_community.retrievers import BM25Retriever, EnsembleRetriever
+from langchain.retrievers import BM25Retriever, EnsembleRetriever
 from langchain.text_splitter import (
     CharacterTextSplitter,
     RecursiveCharacterTextSplitter,
     TokenTextSplitter,
 )
-from langchain.vectorstores import Pinecone
+#from langchain.vectorstores import Pinecone
+from langchain_community.vectorstores import Pinecone
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
@@ -42,18 +44,18 @@ from langchain.prompts import PromptTemplate
 # Suppress warnings
 warnings.filterwarnings("ignore")
 
+print(torch.cuda.is_available()) 
 
 # Automatically find and load the .env file
 load_dotenv(find_dotenv())
 
 
-
 class MedicalChatbot:
-    def __init__(self,cfg):
+    def __init__(self, cfg):
         self.device = torch.device(
-            "cuda" if torch.cuda.is_available()
-            else "mps" if torch.backends.mps.is_built() 
-            else "cpu"
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps" if torch.backends.mps.is_built() else "cpu"
         )
         self.chat_history = []
         self.cfg = cfg
@@ -61,44 +63,43 @@ class MedicalChatbot:
 
     def generate_response(self, user_query):
         response = self.qa_chain({"query": user_query})
-        # self.chat_history.append({"query": user_query, "response": response})
         self.chat_history.append(response)
         return response
 
     def init_embedding_model(self):
         model_kwargs = {"device": self.device}
         encode_kwargs = {"normalize_embeddings": True}
-        self.embed_model = HuggingFaceEmbeddings(
-            self.cfg['embedding_model'],
+        return HuggingFaceEmbeddings(
+            model_name=self.cfg["embedding_model"],
             model_kwargs=model_kwargs,
             encode_kwargs=encode_kwargs,
         )
 
     def load_bm25_retriever(self):
-        chunk_path = self.cfg['retriever']['bm25']['path']
-        topk = self.cfg['retriever']['bm25']['topk']
+        chunk_path = self.cfg["retrievers"]["bm25"]["path"]
+        topk = self.cfg["retrievers"]["bm25"]["topk"]
         with open(chunk_path, "rb") as file:
             chunked_docs = pickle.load(file)
         return BM25Retriever.from_documents(chunked_docs, k=topk)
 
     def load_retrievers(self, name):
-        if name in ['faiss', 'pinecone']:
-            return self.load_db_retriever()
+        if name in ["faiss", "pinecone"]:
+            return self.load_db_retriever(name)
         elif name == "bm25":
             return self.load_bm25_retriever()
 
-    def load_db_retriever(self, retriever_name, faiss_indices_path):
+    def load_db_retriever(self, retriever_name):
         if retriever_name == "pinecone":
             return self.load_pinecone_db_retriever()
         else:
-            return self.load_faiss_db_retriever(faiss_indices_path)
+            return self.load_faiss_db_retriever()
 
     def load_faiss_db_retriever(self):
-        faiss_index_path = f"{self.cfg['retriever']['faiss']['faiss_indices_path']}{self.cfg['embedding_model']}"
-        db = FAISS.load_local(faiss_index_path)
+        faiss_index_path = f"{self.cfg['retrievers']['faiss']['faiss_index_path']}{self.cfg['embedding_model']}"
+        db = FAISS.load_local(faiss_index_path, self.init_embedding_model())
         return db.as_retriever(
             search_type="similarity",
-            search_kwargs={"k": self.cfg['retriever']['faiss']['topk']},
+            search_kwargs={"k": self.cfg["retrievers"]["faiss"]["topk"]},
         )
 
     def load_pinecone_db_retriever(self):
@@ -108,18 +109,24 @@ class MedicalChatbot:
                 "Pinecone API key not found. Please set it in the .env file."
             )
         pc = pinecone.init(api_key=pinecone_api_key)
-        index = pc.Index(self.cfg["retriever"]["pinecone"]["index_name"])
+        index = pc.Index(self.cfg["retrievers"]["pinecone"]["index_name"])
         db = Pinecone(index, self.embed_model, "text")
-        return db.as_retriever(search_type=self.cfg["retriever"]["pinecone"]["search_type"], 
-                               search_kwargs={"k": self.cfg["retriever"]["pinecone"]["topk"]})
+        return db.as_retriever(
+            search_type=self.cfg["retrievers"]["pinecone"]["search_type"],
+            search_kwargs={"k": self.cfg["retrievers"]["pinecone"]["topk"]},
+        )
 
     def load_ensemble_retriever(self):
         ensemble_list = []
-        for retriever in self.cfg["retrievers"]:
-            ensemble_list.append(self.load_retrievers(retriever))
+        weights = self.cfg["ensemble"]["weights"]
+        for name, value in self.cfg["retrievers"].items():
+            retriever = self.load_retrievers(name)
+            ensemble_list.append(retriever)
 
+        if not ensemble_list:
+            raise ValueError("No valid retrievers were loaded.")
         return EnsembleRetriever(
-            retrievers=ensemble_list, weights=self.cfg["ensemble"]["weights"]
+            retrievers=ensemble_list, weights=weights
         )
 
     def init_model(self):
@@ -130,35 +137,42 @@ class MedicalChatbot:
                 bnb_4bit_compute_dtype=torch.float16,
                 bnb_4bit_quant_type="nf4",
                 bnb_4bit_use_double_quant=True,
+                # load_in_8bit=True,
+                # load_in_8bit_fp32_cpu_offload=True,  # Enable CPU offloading
+                # bnb_8bit_compute_dtype=torch.float32,  # Use FP32 for computation when offloaded
             )
         return AutoModelForCausalLM.from_pretrained(
             self.cfg['llm_model']['name'],
             torch_dtype=torch.float16 if self.device.type == "cuda" else "auto",
+            #torch_dtype=torch.float32 if self.device.type == "cuda" else "auto",
             device_map="auto",
             trust_remote_code=True,
             quantization_config=quantization_config if quantization_config else None,
         )
 
     def init_tokenizer(self):
-        return AutoTokenizer.from_pretrained(self.cfg['llm_model']['name'], use_fast=True)
+        return AutoTokenizer.from_pretrained(
+            self.cfg["llm_model"]["name"], use_fast=True
+        )
 
     def init_llm_pipeline(self):
-        generation_config = GenerationConfig.from_pretrained(
-            self.cfg['llm_model']['name'],
-            max_new_tokens=512,
-            temperature=self.cfg['llm_model']['temperature'],
-            top_p=self.cfg['llm_model']['top_p'],
-            do_sample=True,
-            repetition_penalty=self.cfg['llm_model']['repetition_penalty'],
-        )
+        generation_config = GenerationConfig.from_pretrained(self.cfg["llm_model"]["name"])
+        generation_config.max_new_tokens=512
+        generation_config.temperature=self.cfg["llm_model"]["temperature"]
+        generation_config.top_p=self.cfg["llm_model"]["top_p"]
+        generation_config.do_sample=True
+        generation_config.repetition_penalty=self.cfg["llm_model"]["repetition_penalty"]
+    
         pipe = pipeline(
             "text-generation",
             model=self.init_model(),
             tokenizer=self.init_tokenizer(),
-            device=self.device,
+            #device=None if self.device=="cuda" else self.device,
             generation_config=generation_config,
         )
-        return HuggingFacePipeline(pipe)
+        return HuggingFacePipeline(
+            pipeline=pipe
+        )
 
     def init_qa_chain(self):
         llm = self.init_llm_pipeline()
@@ -170,12 +184,21 @@ class MedicalChatbot:
             return_source_documents=True,
             chain_type_kwargs={"prompt": prompt},
         )
-    
+
+
     def get_prompt(self):
+        template = """<s> [INST] You are a helpful assistant for biomedical question-answering tasks. 
+                    Use only the following retrieved context to answer the question. If the answer is not in the context or 
+                    if you don't know the answer, just say that you don't know. 
+                    Provide a response strictly based on the information requested in the query.[/INST] </s> 
+                    [INST] Question: {question} 
+                    Context: {context} 
+                    Answer: [/INST]"""
+        prompt = PromptTemplate.from_template(template)
+        print(prompt)
+        return prompt
         #return hub.pull("rlm/rag-prompt", api_url="https://api.hub.langchain.com")
-        return {"prompt":PromptTemplate.from_template(self.cfg['prompt_template'])}
-
-
+        
     def clean_chat_history(self):
         self.chat_history = []
 
@@ -186,6 +209,31 @@ if __name__ == "__main__":
     with open("cfg.yaml", "r") as file:
         cfg = yaml.safe_load(file)
 
+
+    queries = [
+        "Is occupational outcome in bipolar disorder predicted by premorbid functioning and intelligence?",
+        "What are the effects of α1-antitrypsin (AAT) treatment on chronic fatigue syndrome (CFS) based on a case study involving a 49-year-old woman?",
+        "What are your favorite comedies?"]
+
     chatbot = MedicalChatbot(cfg)
-    response = chatbot.generate_response(str(input()))
-    print(response)
+
+    #Open a Markdown file for writing the results
+    with open("chatbot_results.md", "w") as md_file:
+        for i, query in enumerate(queries):
+            start_time = time.time()  # Start timing
+            result = chatbot.generate_response(query)
+            execution_time = time.time() - start_time  # Calculate execution time
+
+            # Write the query, execution time, and result to the Markdown file
+            md_file.write(f"## Query {i}:\n*{query}*\n\n")
+            md_file.write(f"**Execution Time:**\n{round(execution_time, 2)} seconds on {chatbot.device} using {cfg['llm_model']['name']}.\n\n")
+            md_file.write(f"### Response:\n{result['result']}\n\n")
+            # Add a horizontal rule for separation between entries
+            md_file.write("---\n\n")
+
+    print("Results have been saved to chatbot_results.md")
+
+
+
+    
+
